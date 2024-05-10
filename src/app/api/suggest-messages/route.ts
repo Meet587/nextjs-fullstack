85a9270f105699ee3c237d17892e6c03018b3e49
Meet Rakholiya <meet@investifyd.com>
2024-05-10 14:02:59 +0530

    add open ai

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,81 @@
+
+// export async function POST(req: Request) {
+//     // const { messages } = await req.json();
+
+//     const modelId = google('models/gemini-pro');
+
+//     const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."
+
+//     const prompt2 = "give me some jockes"
+
+//     try {
+//         const result = await streamText({
+//             model: modelId,
+//             maxTokens: 10,
+//             prompt: prompt2,
+//         });
+
+//         console.log(result.textStream)
+
+//         const data = new StreamData();
+
+//         data.append({ test: 'value' });
+
+//         const stream = result.toAIStream({
+//             onFinal(_) {
+//                 data.close();
+//             },
+//         });
+
+//         return new StreamingTextResponse(stream, {}, data);
+//     } catch (error) {
+//         // if (error instanceof OpenAI.APIError) {
+//         //     const { name, status, headers, message } = error
+//         //     return Response.json({ name, status, headers, message }, { status })
+//         // } else {
+//         //     console.error("error occur in ai.", error)
+//         //     throw error
+//         // }
+//         console.error("error occur in ai.", error)
+//         throw error
+//     }
+// }
+
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  try {
+    const prompt =
+      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+    const response = await openai.completions.create({
+      model: 'gpt-3.5-turbo-instruct',
+      max_tokens: 400,
+      stream: true,
+      prompt,
+    });
+
+    const stream = OpenAIStream(response);
+    
+    
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      // OpenAI API error handling
+      const { name, status, headers, message } = error;
+      return NextResponse.json({ name, status, headers, message }, { status });
+    } else {
+      // General error handling
+      console.error('An unexpected error occurred:', error);
+      throw error;
+    }
+  }
+}
